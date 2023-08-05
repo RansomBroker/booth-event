f8ff127837ba79c131f31f8d620e87a4974f795d
RansomBroker <yadisoke@gmail.com>
2023-08-06 00:49:29 +0700

    memperbaiki name convetion

--- database/migrations/2023_08_05_172215_create_redeem_codes_table.php
+++ database/migrations/2023_08_05_172215_create_redeem_codes_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('redeems', function (Blueprint $table) {
+        Schema::create('redeem_codes', function (Blueprint $table) {
             $table->id();
             $table->foreignId("user_id")
                 ->references("id")
